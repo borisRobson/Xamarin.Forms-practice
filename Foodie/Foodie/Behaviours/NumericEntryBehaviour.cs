ba755d0de1b2bc5824b323ff46597a3be92b48c4
Brandon RobsonClarke <Brandon.RobsonClarke@paxton-access.co.uk>
2017-05-16 16:28:26 +0100

    first commit, UI responds to triggers & setters

---
+++ Foodie/Foodie/Behaviours/NumericEntryBehaviour.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using Xamarin.Forms;
+
+namespace Foodie
+{
+    public class NumericEntryBehaviour : Behavior<Entry>
+    {
+        protected override void OnAttachedTo(Entry bindable)
+        {
+            base.OnAttachedTo(bindable);
+            bindable.TextChanged += Bindable_TextChanged;
+        }
+
+        void Bindable_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            //return if no value
+            if (string.IsNullOrWhiteSpace(e.NewTextValue))
+                return;
+            double _;
+            if (!double.TryParse(e.NewTextValue, out _))
+                ((Entry)sender).Text = e.OldTextValue;
+        }
+
+        protected override void OnDetachingFrom(Entry bindable)
+        {
+            base.OnDetachingFrom(bindable);
+            bindable.TextChanged -= Bindable_TextChanged;
+        }
+    }
+}
---
+++ Foodie/Foodie/Main.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Diagnostics;
+using Xamarin.Forms;
+namespace Foodie
+{
+    public class App : Application
+    {
+        public App()
+        {
+            MainPage = new NavigationPage(new EditRecipePage());
+        }
+    }
+
+}
